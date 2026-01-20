21a3e4884100ac3f8232bcd04afc8a13a543d472
Divya-Jain07 <djain6756@gmail.com>
2026-01-21 01:06:44 +0530

    Added the email valid check and js interaction

--- authentication/views.py
+++ authentication/views.py
@@ -4,6 +4,7 @@ import json,re
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.models import User
+from validate_email import validate_email
 # Create your views here.
 
 
@@ -12,12 +13,25 @@ class UsernameValidationView(View):
     @csrf_exempt # <--- This allows Postman to "talk" to this view
     def post(self,request):
         data=json.loads(request.body)
+        
         username=data['username']
         if not str(username).isalnum():
             return JsonResponse({'username_error':'username should only contain alphanumeric characters'},status=400)
         if User.objects.filter(username=username).exists():
             return JsonResponse({'username_error':'sorry username in use,choose another one'},status=409)
         return JsonResponse({'username_valid':True})
+    
+class emailValidation(View):
+    def post(self,request):
+        data=json.loads(request.body)
+        
+        email=data['email']
+        if not validate_email(email):
+            return JsonResponse({'email_error':'email is invalid'},status=400)
+        
+        if User.objects.filter(email=email).exists():
+            return JsonResponse({'email_error':'sorry email in use,choose another one'},status=409)
+        return JsonResponse({'email_valid':True})
 
     
 class RegistrationView(View):
--- financewebsite/static/js/register.js
+++ financewebsite/static/js/register.js
@@ -3,10 +3,16 @@ console.log("Register working")
 
 const usernameField=document.querySelector('#usernameField')
 const feedbackField=document.querySelector('.invalid-feedback')
+const emailField=document.querySelector('#emailField')
+const emailFeedback=document.querySelector('.emailFeedback')
+const usernameSuccessOutput=document.querySelector('.usernameSuccessOutput')
+const emailSuccessOutput=document.querySelector('.emailSuccessOutput')
+
 
 usernameField.addEventListener("keyup", (e)=> { //here e is the element typed on the keyboard
     const usernameVal=e.target.value
-
+    usernameSuccessOutput.style.display='block'
+    usernameSuccessOutput.textContent=`Checking ${usernameVal}...}`
     if(usernameVal.length>0){
     fetch("/authentication/validate-username",{
         body: JSON.stringify({username:usernameVal}),
@@ -17,6 +23,7 @@ usernameField.addEventListener("keyup", (e)=> { //here e is the element typed on
         method:"POST",
     }).then((res)=>res.json()).then((data)=>{
         console.log('data',data)
+        usernameSuccessOutput.style.display='none'
         if(data.username_error)
         {
                 usernameField.classList.add('is-invalid')
@@ -31,4 +38,34 @@ usernameField.addEventListener("keyup", (e)=> { //here e is the element typed on
     });
 }
     });
+
+
+emailField.addEventListener("keyup",(e)=>
+{
+    const emailVal=e.target.value
+    emailSuccessOutput.style.display='block'
+    emailSuccessOutput.textContent=`Checking ${emailVal}...}`
+    if(emailVal.length>0)
+    {
+        fetch("/authentication/validate-email",{
+            body: JSON.stringify({email:emailVal}),
+            method:"POST"
+        }).then((res)=>res.json()).then((data)=>{
+            console.log('data',data)
+            emailSuccessOutput.style.display='none'
+            if(data.email_error)
+            {
+                emailField.classList.add('is-invalid')
+                emailFeedback.style.display='block'
+                emailFeedback.innerHTML= `<p>${data.email_error}</p>`
+
+            }
+            else{
+                emailField.classList.remove('is-invalid');
+                emailFeedback.style.display='none';
+                emailFeedback.innerHTML='';
+            }
+        })
+    }
+})
 //when types
